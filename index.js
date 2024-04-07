5f0372ca81323b98613544f123e4403348825544
[Akseer Ahmad] <68474681+theakseer@users.noreply.github.com>
2024-04-07 19:53:25 +0530

    changed

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const dotenv = require('dotenv');
 
 
 dotenv.config()
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 8000; 
 const app = express();
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 app.use(express.json()); // Parse JSON bodies
